62674beac71c9743c4341148026faaffebe9d23c
JudythG <47835823+JudythG@users.noreply.github.com>
2019-04-14 20:35:03 -0400

    Delete ch1.py
    This was merely a file to test github functionality. Doesn't belong here.

--- ch1.py
+++
@@ -1,8 +0,0 @@
-# Assignment:
-#  Write a program that couns to 100 skipping all odd numbers, printing each
-#  value on after another (use the end= attribute for calls to print())
-
-r = range (1,100,2)
-#print r
-for n in range (1, 100, 2):
-    print (n, end=' ')
